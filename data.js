8e59f913d5d98224d23d2519339909a34d18c9d1
ahmedtsl <ahmedtrying72@gmail.com>
2026-07-04 02:51:04 +0300

    Initial physics booking website

---
+++ data.js
@@ -0,0 +1,8 @@
+const DEFAULT_GROUPS = [
+  { id: 'om-khenan-a', area: 'أم خنان', name: 'مجموعة A', time: '8 م', limit: 125, isClosed: false },
+  { id: 'om-khenan-b', area: 'أم خنان', name: 'مجموعة B', time: '10 م', limit: 125, isClosed: false },
+  { id: 'om-khenan-c', area: 'أم خنان', name: 'مجموعة C', time: '12 ص', limit: 125, isClosed: false },
+  { id: 'istinha-a', area: 'اسطنها', name: 'مجموعة A', time: '3 م', limit: 80, isClosed: false },
+  { id: 'istinha-b', area: 'اسطنها', name: 'مجموعة B', time: '5 م', limit: 80, isClosed: false },
+  { id: 'ashleem-a', area: 'أشليم', name: 'مجموعة A', time: '7:30 م', limit: 100, isClosed: false }
+];
